6e14188f78a33b6c0b610b6363a737cce99ba693
zoran122 <zuzer123456789@gmail.com>
2026-09-13 17:10:43 +0200

    spring boot és mysql kapcsolat + teszt
    Spring Boot projekt beállítása MySQL-kapcsolattal, majd a kapcsolat ellenőrzése egy Ping teszt entitással.

---
+++ cleanride/src/main/java/com/cleanride/cleanride/CleanrideApplication.java
@@ -0,0 +1,13 @@
+package com.cleanride.cleanride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanrideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanrideApplication.class, args);
+	}
+
+}
