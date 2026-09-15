f40ab7c4e90bf2dfb54485e58ba463d5644e5f83
zoran122 <zuzer123456789@gmail.com>
2026-09-15 11:07:36 +0200

    Delete cleanride_backend directory

--- cleanride_backend/src/main/java/com/cleanride/cleanride/repository/UsersRepository.java
+++
@@ -1,5 +0,0 @@
-package com.cleanride.cleanride.repository;
-import com.cleanride.cleanride.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> { }
