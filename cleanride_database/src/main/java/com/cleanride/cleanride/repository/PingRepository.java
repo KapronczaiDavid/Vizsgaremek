7370674cb97771b124789d2339286659ac179bc5
zoran122 <zuzer123456789@gmail.com>
2026-09-15 12:52:36 +0200

    renamed cleanride_database to clenride_backend

--- cleanride_database/src/main/java/com/cleanride/cleanride/repository/PingRepository.java
+++
@@ -1,4 +0,0 @@
-package com.cleanride.cleanride.repository;
-import com.cleanride.cleanride.entity.Ping;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface PingRepository extends JpaRepository<Ping, Long> { }
